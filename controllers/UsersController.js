bfa9fb5f9f8ed0b3cd19296c9bd1234e80fb53fc
Domengo <dominicsengo@gmail.com>
2023-06-27 14:44:47 +0300

    users

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,7 +1,17 @@
+import sha1 from 'sha1';
+import Queue from 'bull';
+// eslint-disable-next-line no-unused-vars
+import { ObjectID } from 'mongodb';
+// eslint-disable-next-line no-unused-vars
+import redisClient from '../utils/redis';
+
 const dbClient = require('../utils/db');
 
+const userQueue = new Queue('userQueue', 'redis://127.0.0.1:6379');
+
 class UsersController {
-  static async postNew(request, response) {
+  // eslint-disable-next-line consistent-return
+  static postNew(request, response) {
     const { email, password } = request.body;
     if (!email) {
       return response.status(400).json({ error: 'Missing email' });
@@ -9,8 +19,25 @@ class UsersController {
     if (!password) {
       return response.status(400).json({ error: 'Missing password' });
     }
-    const user = await dbClient.nbUsers();
-    return response.status(201).json({ id: user.id, email: user.email });
+
+    const users = dbClient.db.collection('users');
+
+    users.findOne({ email }, (err, result) => {
+      if (err) throw err;
+      if (result) {
+        response.status(400).json({ error: 'Already exist' });
+      } else {
+        const hashedPassword = sha1(password);
+        users.insertOne({ email, password: hashedPassword })
+          .then((result) => {
+            response.status(201).json({ id: result.insertedId, email });
+            userQueue.add({ userId: result.insertedId });
+          }).catch((error) => {
+            console.log(error);
+          });
+      }
+    });
   }
 }
+
 module.exports = UsersController;
