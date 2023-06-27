f277c419a50fe9de797f981afcc7d70dbf57a3e8
Domengo <dominicsengo@gmail.com>
2023-06-27 11:35:38 +0300

    mongo

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,35 @@
 import { MongoClient } from 'mongodb';
 
-const url = 'mongodb://localhost'
-const client = new MongoClient(url);
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`;
+    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+      if (!err) {
+        this.db = client.db(database);
+        this.users = this.db.collection('users');
+        this.files = this.db.collection('files');
+      } else {
+        console.log(err.message);
+        this.db = false;
+      }
+    });
+  }
 
-client.connect();
+  isAlive() {
+    return !!this.db;
+  }
+
+  async nbUsers() {
+    return this.users.countDocuments();
+  }
+
+  async nbFiles() {
+    return this.files.countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
