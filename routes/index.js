66e087bbb905c7e1b1718eb7002a2557a66f8681
Domengo <dominicsengo@gmail.com>
2023-06-28 11:46:16 +0300

    create a new user

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.get('/users', UsersController.postNew);
+router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
